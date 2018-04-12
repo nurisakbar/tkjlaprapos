9dd41de6c383b7ff0b00a414ff66ef8574020d21
nuris akbar <nuris.akbar@gmail.com>
2018-04-12 16:30:44 +0700

    first commit

---
+++ database/migrations/2018_04_12_070301_create_table_product.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableProduct extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('category_id');
+            $table->integer('brand_id');
+            $table->integer('price');
+            $table->integer('stock');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            //
+        });
+    }
+}
---
+++ resources/views/category/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layout')
+@section('content')
+    <table class="table table-bordered">
+        <tr>
+            <th>No</th>
+            <th>Category Name</th>
+            <th>Action</th>
+        </tr>
+    </table>
+@endsection
